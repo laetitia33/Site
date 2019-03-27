9f713eb59243ba26f2d0821661cb71f9679eef66
Laetitia Bernardi <33260laetitia.bernardi@gmail.com>
2019-03-27 08:16:11 +0100

    page rdv

--- views/template.php
+++ views/template.php
@@ -9,8 +9,8 @@
 		<?php include_once 'views/include/menu.php'; ?>
 		<?php  include_once 'views/include/menu_responsive.php'; ?>
 	</header>
- <div id="content"> 
-		<div id="blocpage">	
+<div id="content">
+<div id="blocpage">	
 		<a id="films"></a>
 	<section>				
 		<?= $content ?>
@@ -26,7 +26,14 @@
 	<script src="assets/js/form.js"></script>
 	<script type="text/javascript" src="assets/js/engine1/wowslider.js"></script>
 	<script type="text/javascript" src="assets/js/engine1/script.js"></script>
-	<script type="text/javascript" src="assets/js/classie.js"></script>
+	<script src="assets/js/cookiechoices.js"></script>
+	<script src="assets/js/classie.js"></script>
+	<script>document.addEventListener('DOMContentLoaded', function(event){cookieChoices.showCookieConsentBar('Ce site utilise des cookies pour vous offrir le meilleur service. En poursuivant votre navigation, vous acceptez l’utilisation des cookies.', 'J’accepte', 'En savoir plus', 'http://www.example.com/mentions-legales/');});</script>
+		<!-- ScheduleOnce button START -->
+	<button id="SOIBTN_LaetitiaBernardi" style="background: #294354; color: #ffffff; padding: 10px px; border: 1px solid #c8c8c8; font: bold 14px Arial;border-radius:100%;padding:20px; cursor: pointer; position: fixed; bottom: 0;left: 0px; z-index: 100 !important;" data-height="580" data-psz="00" data-so-page="LaetitiaBernardi" data-delay="1">Planifiez <br>un <br>rendez-vous</button>
+	<script type="text/javascript" src="https://cdn.oncehub.com/mergedjs/so.js"></script>
+	<!-- ScheduleOnce button END -->
+	
 
 </div>
 </div>

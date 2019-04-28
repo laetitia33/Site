8e3ef32945bf2462186194a23d83ca697e57393d
Laetitia Bernardi <33260laetitia.bernardi@gmail.com>
2019-04-28 19:18:20 +0200

    juste avant de faire slideshow

--- views/serviceDetail.php
+++ views/serviceDetail.php
@@ -1,6 +1,8 @@
 <?php $title =  'Création de sites internet'; ?>
 
 
+
+
 <!DOCTYPE html>
 <html lang="fr" class='template'>
 		<head>
@@ -48,15 +50,15 @@
 			{ window.sidebar.addPanel("<a href="javascript:void(favoris());">Assitance Informatique Bassi d'Arcachon</a>","http://www.tonsite.com",""; }
 			else { window.external.AddFavorite("http://www.tonsite.com","le nom du site"); } 
 		</script>
-
 		<!--fenetre de confirmation-->
 		<link data-require="sweet-alert@*" data-semver="0.4.2" rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/sweetalert/1.1.3/sweetalert.css" />
 		<script src="https://unpkg.com/sweetalert/dist/sweetalert.min.js"></script>
 
+		<link rel="stylesheet" type="text/css" href="assets/js/pageSites/css/animate.css" />
+		<link rel="stylesheet" type="text/css" href="assets/js/pageSites/css/mockup4.css" />
+
 		<title><?= isset($title) ? $title : 'Création de sites internet';?></title>
 	</head>
-
-
 <body class="demo-3">
 		<div id="container" class="container intro-effect-sliced">
 			<!-- Top Navigation -->
@@ -73,7 +75,6 @@
 						<a href="index6.html">Grid</a>
 						<a href="index7.html">Jam 3</a>
 					</nav>
-
 					<h1>Développeur intégrateur en réalisation d’applications web</h1>
 					<p class="subline">Inspiration for Article Intro Effects</p>
 					<p>by <strong>Eric Heathway</strong> &#xf063; Posted in <strong>Ethics</strong> on <strong>May 18, 2014</strong></p>
@@ -105,47 +106,27 @@
 					<h3>From acid and rancidity</h3>
 					<p>The most esteemed and dearest oil is Almond. What is called Peach-kernel oil (Oleum Amygdalæ impart a richer flavour.</p>
 					<p>The solid fats sold as butter and lard substitutes, consist of deodorised cocoanut oil, and they are excellent for cooking purposes. It is claimed that biscuits, &amp;c., made from them may be kept for a much longer period, without showing any trace of rancidity, than if butter or lard had been used. They are also to be had agreeably flavoured by admixture with almond, walnut, &amp;c., "cream."</p>
-
 					<p>The better quality oils are quite as wholesome as the best fresh butter, and better than most butter as sold. Bread can be dipped into the oil, or a little solid vegetable fat spread on it. The author prefers to pour a little Peach-kernel oil upon some ground walnut kernels (or other ground nuts in themselves rich in oil), mix with a knife to a suitable consistency and spread upon the bread. Pine-kernels are very oily, and can be used in pastry in the place of butter or lard.</p>
-
 					<p>Whenever oils are mentioned, without a prefix, the fixed or fatty oils are always understood. The volatile or essential oils are a distinct class. Occasionally, the fixed oils are called hydrocarbons, but hydrocarbon oils are quite different and consist of carbon and hydrogen alone. Of these, petroleum is incapable of digestion, whilst others are poisonous.</p>
-
 					<p>Vegetable Acids are composed of the same three elements and undergo combustion into the same compounds as the carbohydrates. They rouse the appetite, stimulate digestion, and finally form carbonates in combination with the alkalies, thus increasing the alkalinity of the blood. The chief vegetable acids are: malic acid, in the apple, pear, cherry, &amp;c.; citric acid, in the lemon, lime, orange, gooseberry, cranberry, strawberry, raspberry, &amp;c.; tartaric acid, in the grape, pineapple, &amp;c.</p>
-
 					<p>Some place these under Class III. or food adjuncts. Oxalic acid (except when in the insoluble state of calcium oxalate), and several other acids are poisonous.</p>
-
 					<h3>The essential part of every living cell</h3>
 					<p>Proteids or Albuminoids are frequently termed flesh-formers. They are composed of nitrogen, carbon,  fat or merely give rise to heat. They form the essential part of every living cell.</p>
-
 					<p>Proteids are excreted from the body as water, carbon dioxide, urea, uric acid, sulphates, &amp;c.</p>
-
 					The principal proteids of animal origin have their corresponding proteids in the vegetable kingdom. Some kinds, whether of animal or vegetable origin, are more easily digested than others. They have the same physiological value from whichever kingdom they are derived.</p>
-
 					<p>The Osseids comprise ossein, gelatin, cartilage, &amp;c., from bone, skin, and connective issue. termed albuminoids to be always spoken of as proteids only.</p>
-
 					<p>Jellies are of little use as food; not only is this because of the low nutritive value of gelatin, but also on account of the small quantity which is mixed with a large proportion of water.</p>
-
 					<p>The Vegetable Kingdom is the prime source of all organic food; water, and to a slight extent salts,  from grass, clover, turnips, &amp;c. All the proteids of the flesh once existed as proteids in the vegetables; some in exactly the same chemical form.</p>
-
 					<blockquote>A comparatively small quantity of proteid matter, such as is easily obtained from vegetable food, is ample for the general needs of the body.</blockquote>
 					<p>Flesh contains no starch or sugar, but a small quantity of glycogen. The fat in an animal is derived converted into peptone by the digestive juices—taking the form of a perfectly diffusible liquid—otherwise they could not be absorbed and utilised by the body. Thus the products of digestion of  sufficient evidence against the earlier view.</p>
-
 					<p>A comparison of flesh and vegetable food, shows in flesh an excessive quantity of proteid matter, a  a large proportion of starch and sugar and a small quantity of fat. Some vegetable foods, particularly nuts, contain much fat.</p>
-
 					<p>Investigation of the digestive processes has shown that the carbohydrates and fats entail little strain on the system; their ultimate products are water and carbon dioxide, which are easily disposed nitrogen, is expelled by the kidneys in the form of urea (a very soluble substance), and a small quantity of uric acid in the form of quadurates.</p>
-
 					<p>There is reciprocity between the teeth and digestive organs of animals and their natural food. The grasses, leaves, &amp;c., which are consumed by the herbivora, contain a large proportion of cellulose which are used in enormous quantities contain aloes or other drugs which stimulate the action of the intestines.</p>
-
 					<p>Highly manufactured foods, from which as much as possible of the non-nutritious matter has been removed is often advocated, generally by those interested in its sale. Such food would be advantageous  all derived from vegetable sources and simply prepared is the rule, diseases of the digestive organs are rare. The Englishman going to a tropical country and partaking largely of flesh and alcohol, suffers from disease of the liver and other organs, to which the natives and the few of his own countrymen, living in accordance with natural laws are strangers.</p>
 					<p><strong>Excerpt from: <a href="http://www.gutenberg.org/ebooks/15237">The Chemistry of Food and Nutrition</a> by A. W. Duncan</strong></p>
-
 				</div>
-
 				
-
 			</article>
-
-
 			
 		</div><!-- /container -->
 				<a href="#" class="scroll-to"><i class="fa fa-chevron-up"></i></a>
@@ -154,7 +135,47 @@
 				<script src ="assets/js/script.js"></script>
 				<script src="assets/js/cookiechoices.js"></script>
 				<script>document.addEventListener('DOMContentLoaded', function(event){cookieChoices.showCookieConsentBar('Ce site utilise des cookies pour vous offrir le meilleur service. En poursuivant votre navigation, vous acceptez l’utilisation des cookies.', 'J’accepte', 'En savoir plus', 'http://www.example.com/mentions-legales/');});</script>
+				<link rel="stylesheet" type="text/css" href="assets/js/pageSites/js/main.js" />
+			<script>
+			(function() {
+				new Slideshow( document.getElementById( 'slideshow-1' ) );
+				setTimeout( function() {
+					new Slideshow( document.getElementById( 'slideshow-2' ) );
+				}, 1750 );
 
+				/* Mockup responsiveness */
+				var body = docElem = window.document.documentElement,
+					wrap = document.getElementById( 'wrap' ),
+					mockup = wrap.querySelector( '.mockup' ),
+					mockupWidth = mockup.offsetWidth;
+
+				scaleMockup();
+
+				function scaleMockup() {
+					var wrapWidth = wrap.offsetWidth,
+						val = wrapWidth / mockupWidth;
+
+					mockup.style.transform = 'scale3d(' + val + ', ' + val + ', 1)';
+				}
+				
+				window.addEventListener( 'resize', resizeHandler );
+
+				function resizeHandler() {
+					function delayed() {
+						resize();
+						resizeTimeout = null;
+					}
+					if ( typeof resizeTimeout != 'undefined' ) {
+						clearTimeout( resizeTimeout );
+					}
+					resizeTimeout = setTimeout( delayed, 50 );
+				}
+
+				function resize() {
+					scaleMockup();
+				}
+			})();
+		</script>
 
 			<script type="text/javascript">
 				//fond du titre
